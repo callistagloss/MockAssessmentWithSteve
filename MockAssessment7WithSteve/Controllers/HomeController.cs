725ee79ca10d0cedb7fa5dd37a63c876e769c11d
Callista <theevenstarlight@gmail.com>
2019-06-09 16:50:00 -0400

    first commit and working

---
+++ MockAssessment7WithSteve/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using MockAssessment7WithSteve.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MockAssessment7WithSteve.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //[HttpPost]
+        public ActionResult Search(int Id)
+        {
+            string donutURL = $"https://grandcircusco.github.io/demo-apis/donuts/{Id}.json";
+
+            HttpWebRequest request = WebRequest.CreateHttp(donutURL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string APIText = rd.ReadToEnd();
+
+            JToken donutToken = JToken.Parse(APIText);
+
+            Doughnut d = new Doughnut();
+
+            d.Name = donutToken["name"].ToString();
+            d.Calories = (int)donutToken["calories"];
+
+            if (donutToken["photo"] != null)
+            {
+                d.PhotoURL = donutToken["photo"].ToString();
+            }
+
+            List<JToken> jt = new List<JToken>();
+
+            List<string> donutExtras = new List<string>();
+
+            if(donutToken["extras"] != null)
+                {
+                foreach(JToken j in donutToken["extras"])
+                {
+                    donutExtras.Add(j.ToString());
+                }
+
+                d.Extras = donutExtras.ToArray();
+
+            }
+                ViewBag.Donut = d;
+                return View(d);
+
+        }
+    }
+}
---
+++ MockAssessment7WithSteve/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MockAssessment7WithSteve.MvcApplication" Language="C#" %>
